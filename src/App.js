f5e52ac2f3409d88734efafba36fed80f3238989
Tej Pratap <kumarpratap.1447@gmail.com>
2021-05-25 18:02:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React,{Fragment} from 'react';
+import { Route,Switch } from "react-router-dom";
+
 import './App.css';
+
+//components files
+import About from './components/About';
+
+//layout files
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+     
+      <div className="container">
+      <Header/>
+        <h1>Hello React</h1>
+        <Switch>
+          <Route exact path='/about'component={About}/>
+        </Switch>
+      <Footer/>
+      </div>
+      
+    </Fragment>
+    
+    
+     
+    
   );
 }
 
